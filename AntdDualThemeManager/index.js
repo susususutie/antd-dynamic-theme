c73b7690e5c912c686aae5b7f5292f3d8af5cc9d
sutie <susususutie@gmail.com>
2025-07-02 15:52:37 +0800

    doc

--- AntdDualThemeManager/index.js
+++ AntdDualThemeManager/index.js
@@ -4,3 +4,10 @@ export { UpdaterContext } from './context/updaterContext'
 export { PrefixContext } from './context/prefixContext'
 export { SeedTokenContext } from './context/seedTokenContext'
 export { ThemeModeContext } from './context/themeModeContext'
+
+/**
+ * TODO:
+ * 1. antd@4 适配度不够
+ * 2. 部分组件暗黑模式样式异常
+ * 3. 加入紧凑主题功能
+ */
